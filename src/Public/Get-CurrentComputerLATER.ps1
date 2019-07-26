20246c942755cbd608c9784e93f636016d4845d1
Philip Haglund <Gonjer@gmail.com>
2019-07-27 00:30:19 +0200

    Fix LAPS Proxy

--- src/Public/Get-CurrentComputerLATER.ps1
+++ src/Public/Get-CurrentComputerLATER.ps1
@@ -1,9 +1,22 @@
 function Get-CurrentComputerLATER {
+    <#
+    .SYNOPSIS
+    Finds admin password for given computer
+
+    .DESCRIPTION
+    Finds local admin password and password expiration timestamp for given computer
+
+    .EXAMPLE
+    Get-CurrentComputerLATER -ComputerName CLIENT012
+
+    Gets password of local administrator on computer CLIENT012
+
+    #>
     [CmdletBinding(
         SupportsShouldProcess
     )]
     param (
-        # Parameter help description
+        # Input a valid Computer Name to request local administrator password.
         [Parameter(
             Mandatory
         )]
@@ -17,19 +30,38 @@ function Get-CurrentComputerLATER {
                 $PSSenderInfo
                 $WSManInstance = Get-WSManInstance -ComputerName localhost -ResourceURI Shell -Enumerate
 
-                #$WSManInstance.ClientIP
-                
-                #Get SQL TABLE current user
-                #compare againast timestamp
-                # if valid timestamps
-                #Request LAPS PW 
-                # else FAIL
-                
-                
+                $BaseQuery = @"
+SELECT TOP 10 [UserId]
+      ,[ComputerName]
+      ,[ComputerIPAddress]
+      ,[Timestamp]
+FROM [Later].[dbo].[Requests] WHERE UserId = 'REPLACEUSERNAME' Order By TimeStamp Desc
+"@
 
+                $SQLQuery = $BaseQuery -replace 'REPLACEUSERNAME', $PSSenderInfo.ConnectedUser
+                [Object[]]$PastLater = Invoke-DbaQuery -SqlInstance localhost -Database Later -Query $SQLQuery
+
+                $TimeStampString = $PastLater.TimeStamp.ForEach( { $_.ToString() })
+                $Now = [datetime]::Now
+                $RequestsToday = ($TimeStampString -replace '\s.*$') -match $Today
+
+                if ($RequestsToday -ge 3) {
+                    throw ('No more requests allowed for {0} today' -f $PSSenderInfo.ConnectedUser)
+                }
+                elseif ($Result[0].Timestamp -ge $Now.AddHours(-1)) {
+                    throw ('Request already submitted for this user, wait time {0} Minutes' -f ($Result[0].Timestamp - $Now.AddHours(-1)).Minutes)
+                }
+                else {
+                    Get-AdmPwdPassword -ComputerName $ComputerName
+                    [PSCustomObject]@{
+                        UserId            = $PSSenderInfo.ConnectedUser
+                        ComputerName      = $ComputerName
+                        ComputerIPAddress = $WSManInstance.ClientIP
+                    } | Write-DbaDbTableData -SqlInstance localhost -Database Later -Table Requests
+                }
             }
             catch {
-
+                $PSCmdlet.ThrowTerminatingError($_)
             }
         }
     }
