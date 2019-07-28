958d1f9eedd9c6ef84e0a4cb3dff6bdc94f400cd
Philip Haglund <Gonjer@gmail.com>
2019-07-29 00:01:04 +0200

    Change Path to PSModulePath for LATER

--- JEA/New-PSRoleCapabilityFile.ps1
+++ JEA/New-PSRoleCapabilityFile.ps1
@@ -1,6 +1,6 @@
 ﻿$CapabilityFileParams = @{
-    'Path'            = "C:\JEA\Requester.psrc"
-    'Author'          = "Omnicit AB" 
+    'Path'            = "C:\Program Files\WindowsPowerShell\Modules\LATER\RoleCapabilities\Requester.psrc"
+    'Author'          = "Omnicit AB"
     'CompanyName'     = 'Omnicit AB'
     'Description'     = 'JEA capability file for LATER.'
     'ModulesToImport' = 'LATER'
