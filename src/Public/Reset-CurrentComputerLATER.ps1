85ec0c41f91585e35167bfc7a45c648ce9a8b468
Philip Haglund <Gonjer@gmail.com>
2019-07-29 00:03:27 +0200

    Inital commit

---
+++ src/Public/Reset-CurrentComputerLATER.ps1
@@ -0,0 +1,36 @@
+﻿function Reset-CurrentComputerLATER {
+    <#
+    .SYNOPSIS
+    Reset admin password for given computer
+
+    .DESCRIPTION
+    Reset local admin password and password expiration timestamp for given computer
+
+    .EXAMPLE
+    Reset-CurrentComputerLATER -ComputerName CLIENT012
+
+    Reset password of local administrator on computer CLIENT012
+
+    #>
+    [CmdletBinding(
+        SupportsShouldProcess
+    )]
+    param (
+        # Input a valid Computer Name to request local administrator password.
+        [Parameter(
+            Mandatory
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]$ComputerName
+    )
+    process {
+        if ($PSCmdlet.ShouldProcess($ComputerName, $MyInvocation.MyCommand.Name)) {
+            try {
+                Reset-AdmPwdPassword -ComputerName $ComputerName
+            }
+            catch {
+                $PSCmdlet.ThrowTerminatingError($_)
+            }
+        }
+    }
+}
