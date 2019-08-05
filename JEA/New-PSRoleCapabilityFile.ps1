d2de46030fda36cbdee291d7567419f127991247
Philip Haglund <philip.haglund@omnicit.se>
2019-08-05 16:44:38 +0200

    Change to VisibleFunctions instead of IPMO

--- JEA/New-PSRoleCapabilityFile.ps1
+++ JEA/New-PSRoleCapabilityFile.ps1
@@ -1,9 +1,9 @@
 ﻿$CapabilityFileParams = @{
-    'Path'            = "C:\Program Files\WindowsPowerShell\Modules\LATER\RoleCapabilities\Requester.psrc"
-    'Author'          = "Omnicit AB"
-    'CompanyName'     = 'Omnicit AB'
-    'Description'     = 'JEA capability file for LATER.'
-    'ModulesToImport' = 'LATER'
+    'Path'             = "C:\Program Files\WindowsPowerShell\Modules\LATER\RoleCapabilities\Requester.psrc"
+    'Author'           = "Omnicit AB"
+    'CompanyName'      = 'Omnicit AB'
+    'Description'      = 'JEA capability file for LATER.'
+    'VisibleFunctions' = @('Get-CurrentComputerLATER', 'Reset-CurrentComputerLATER')
 }
 
 New-PSRoleCapabilityFile @CapabilityFileParams
