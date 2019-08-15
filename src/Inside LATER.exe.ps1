5819fbaba9463c8382ec3161cdd85637b83cd3af
Philip Haglund <philip.haglund@omnicit.se>
2019-08-15 21:01:34 +0200

    Add whoami /groups check

--- src/Inside LATER.exe.ps1
+++ src/Inside LATER.exe.ps1
@@ -4,19 +4,30 @@
     try {
         $progressbaroverlay1.Visible = $true
         try {
+            [string[]]$Group = ((whoami /groups /fo csv | ConvertFrom-Csv -Delimiter ',' -ErrorAction Stop).'Group Name' -match '^(?:.+\\)(res-sys-later\spol\d+$)' -replace '^(?:.+\\)')
+            if ($Group.Count -eq 0) {
+                throw [System.Management.Automation.RemoteException]::New('Missing policy groups to request administrator access, contact support.')
+            }
+            $progressbaroverlay1.Value = 10
+        }
+        catch {
+            throw [System.Management.Automation.RemoteException]::New('Unable to retrieve group membership.')
+        }
+        try {
             $SessionOption = New-PSSessionOption -IdleTimeout 60000 -ErrorAction Stop
             $Session = New-PSSession -ComputerName $LATERServer -ConfigurationName LATER -SessionOption $SessionOption -ErrorAction Stop
+            $progressbaroverlay1.Value = 20
         }
         catch {
             throw [System.AccessViolationException]::New('Unable to connect to request server')
         }
-        $progressbaroverlay1.Value = 10
 
-        # Retrive the current LAPS password for $Env:ComputerName using Invoke-Command. Enter-PSSession and Import-PSSession is unavailable because of the JEA Configuration.
+        # Retrieve the current LAPS password for $Env:ComputerName using Invoke-Command. Enter-PSSession and Import-PSSession is unavailable because of the JEA Configuration.
         try {
             $Later = Invoke-Command -Session $Session -ScriptBlock {
                 Get-CurrentComputerLATER -ComputerName $using:env:COMPUTERNAME -ErrorAction Stop
             } -ErrorAction Stop
+            $progressbaroverlay1.Value = 30
         }
         catch [System.Management.Automation.Runspaces.InvalidRunspaceStateException] {
             throw [System.Management.Automation.Runspaces.InvalidRunspaceStateException]::New('Restart the Later application and try again')
@@ -27,11 +38,10 @@
         catch {
             throw [System.InvalidOperationException]::New('Something went wrong, contact administrator with the current timestamp {0}' -f ([datetime]::Now.ToString()))
         }
-        $progressbaroverlay1.Value = 20
 
         # Update Group Policy to allow for 90 (with +- 30 minutes offset time) of administrator time.
         $null = gpupdate.exe /force
-        $progressbaroverlay1.Value = 30
+        $progressbaroverlay1.Value = 40
         <#
             As encoded command
             Add-LocalGroupMember -Group Administrators -Member 'NT Authority\Interactive';
@@ -40,18 +50,18 @@
         #>
         $Command = 'QQBkAGQALQBMAG8AYwBhAGwARwByAG8AdQBwAE0AZQBtAGIAZQByACAALQBHAHIAbwB1AHAAIABBAGQAbQBpAG4AaQBzAHQAcgBhAHQAbwByAHMAIAAtAE0AZQBtAGIAZQByACAAJwBOAFQAIABBAHUAdABoAG8AcgBpAHQAeQBcAEkAbgB0AGUAcgBhAGMAdABpAHYAZQAnADsADQAKAEEAZABkAC0AVAB5AHAAZQAgAC0AQQBzAHMAZQBtAGIAbAB5AE4AYQBtAGUAIABTAHkAcwB0AGUAbQAuAFcAZQBiADsADQAKAFMAZQB0AC0ATABvAGMAYQBsAFUAcwBlAHIAIAAtAE4AYQBtAGUAIABBAGQAbQBpAG4AaQBzAHQAcgBhAHQAbwByACAALQBQAGEAcwBzAHcAbwByAGQAIAAoAFsAUwB5AHMAdABlAG0ALgBXAGUAYgAuAFMAZQBjAHUAcgBpAHQAeQAuAE0AZQBtAGIAZQByAHMAaABpAHAAXQA6ADoARwBlAG4AZQByAGEAdABlAFAAYQBzAHMAdwBvAHIAZAAoADIANAAsACAANQApACAAfAAgAEMAbwBuAHYAZQByAHQAVABvAC0AUwBlAGMAdQByAGUAUwB0AHIAaQBuAGcAIAAtAEEAcwBQAGwAYQBpAG4AVABlAHgAdAAgAC0ARgBvAHIAYwBlACkA'
         $Cred = [pscredential]::new('Administrator', (ConvertTo-SecureString -String $Later.Password -AsPlainText -Force))
-        $progressbaroverlay1.Value = 40
-        Start-Process -FilePath PowerShell.exe -ArgumentList "-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -EncodedCommand $Command" -Credential $Cred
         $progressbaroverlay1.Value = 50
+        Start-Process -FilePath PowerShell.exe -ArgumentList "-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -EncodedCommand $Command" -Credential $Cred
+        $progressbaroverlay1.Value = 60
 
         # Update password for LAPS to avoid reuse, will be applied after next gpupdate.
         $Later = Invoke-Command -Session $Session -ScriptBlock {
             Reset-CurrentComputerLATER -ComputerName $using:env:COMPUTERNAME -ErrorAction SilentlyContinue
         } -ErrorAction SilentlyContinue
-        $progressbaroverlay1.Value = 60
+        $progressbaroverlay1.Value = 70
 
         Remove-PSSession -Session $Session -Confirm:$false -ErrorAction SilentlyContinue
-        $progressbaroverlay1.Value = 70
+        $progressbaroverlay1.Value = 80
 
         # Verify that LATER is correctly applied
         $Count = 0
@@ -68,7 +78,7 @@
                     }
                 }
             }
-            $progressbaroverlay1.Value = 80
+            $progressbaroverlay1.Value = 90
             if ($Members.Name -contains 'NT AUTHORITY\INTERACTIVE') {
                 # Success
                 $progressbaroverlay1.Value = 100
