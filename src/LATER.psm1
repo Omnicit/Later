c4fc17cd9b730d952b5a88e6ddef10bc22fd62c1
Philip Haglund <philip.haglund@omnicit.se>
2019-08-16 12:01:54 +0200

    Add script variables

--- src/LATER.psm1
+++ src/LATER.psm1
@@ -1,7 +1,16 @@
-#Unblock files.
+[System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
+# Script variables used to point ut the SQL Instance
+[string]$Database = 'Later'
+[string]$Schema = 'dbo'
+[string]$SqlInstance = 'localhost'
+
+# All L.A.T.E.R Active Directory Groups are named with the prefix 'res-sys-later'
+
+# Unblock files.
 Get-ChildItem -Path $PSScriptRoot -Recurse | Unblock-File
 
-#Dot source private functions.
-Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 | Foreach-Object { . $_.FullName }
-#Dot source public functions.
-Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 | Foreach-Object { . $_.FullName }
+# Dot source private functions.
+Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 | ForEach-Object { . $_.FullName }
+# Dot source public functions.
+Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 | ForEach-Object { . $_.FullName }
+
