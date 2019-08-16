7f0adba7a06aeb814e6f33bdfe8f2cb41e75f62d
Philip Haglund <philip.haglund@omnicit.se>
2019-08-16 12:02:02 +0200

    Change to SID

--- src/Public/Get-CurrentComputerLATER.ps1
+++ src/Public/Get-CurrentComputerLATER.ps1
@@ -24,44 +24,30 @@ function Get-CurrentComputerLATER {
         [string]$ComputerName
     )
     begin {
-        [string]$Database = 'Later'
-        [string]$Schema = 'dbo'
-        [string]$SqlInstance = 'localhost'
         [string]$TablePolicy = 'Policy'
         [string]$TableRequests = 'Requests'
         [string]$TableFailedRequests = 'FailedRequests'
-        # All L.A.T.E.R Active Directory Groups are named with the prefix 'res-sys-later'
-    }
-    process {
-        if ($PSCmdlet.ShouldProcess($ComputerName, $MyInvocation.MyCommand.Name)) {
-            try {
-                $WSManInstance = Get-WSManInstance -ConnectionURI $PSSenderInfo.ConnectionString -ResourceURI shell -Enumerate -ErrorAction Stop
-                try {
-                    $ComputerNameByAddress = ([System.Net.Dns]::GetHostByAddress($WSManInstance.ClientIP).HostName)
-                    # Preferably log when $ComputerNameByAddress -notmatch $ComputerName
-                }
-                catch {
-                    $ComputerNameByAddress = 'NULL'
-                }
 
-                $User = Get-ADUser -Identity ($PSSenderInfo.ConnectedUser -replace '^(?:.+\\)') -ErrorAction Stop
-                $UserPolicyGroups = Get-ADPrincipalGroupMembership -Identity $User -ErrorAction Stop | Where-Object { $_.SamAccountName -match '^(res-sys-later.*$)' }
-                $Request = [PSCustomObject]@{
-                    UserId                = $User.ObjectGUID.Guid
-                    ComputerName          = $ComputerName
-                    ComputerNameByAddress = $ComputerNameByAddress
-                    ComputerIPAddress     = $WSManInstance.ClientIP
-                }
-
-                $BasePolicyQuery = @"
+        $BasePolicyQuery = @"
 SELECT [Id]
       ,[GroupId]
       ,[Computers]
       ,[TimesPerDay]
-FROM [$Database].[$Schema].[$TablePolicy] WHERE GroupId IN (REPLACEGUID)
+FROM [$Database].[$Schema].[$TablePolicy] WHERE GroupId IN (REPLACESID)
 "@
+        $BasePastLaterQuery = @"
+SELECT TOP 1000 [UserId]
+      ,[ComputerName]
+      ,[Timestamp]
+FROM [$Database].[$Schema].[$TableRequests] WHERE UserId = 'REPLACESID' Order By TimeStamp Desc
+"@
+    }
+    process {
+        if ($PSCmdlet.ShouldProcess($ComputerName)) {
+            try {
+                $Request = Get-LaterRequesterInfo -ComputerName $ComputerName -ErrorAction Stop
 
-                $PolicySQLQuery = $BasePolicyQuery -replace 'REPLACEGUID', ("'{0}'" -f ($UserPolicyGroups.objectGUID.Guid -join "', '"))
+                $PolicySQLQuery = $BasePolicyQuery -replace 'REPLACESID', ("'{0}'" -f ($Request.UserPolicyGroups -join "', '"))
                 try {
                     [Object[]]$Policies = Invoke-DbaQuery -SqlInstance $SqlInstance -Database $Database -Query $PolicySQLQuery -ErrorAction Stop | Sort-Object -Property Computer
                     $Policy = $Policies[0]
@@ -71,18 +57,13 @@ FROM [$Database].[$Schema].[$TablePolicy] WHERE GroupId IN (REPLACEGUID)
                     throw [System.AccessViolationException]::New('No L.A.T.E.R policy found for user.')
                 }
 
-                $BasePastLaterQuery = @"
-SELECT TOP 1000 [UserId]
-      ,[ComputerName]
-      ,[Timestamp]
-FROM [$Database].[$Schema].[$TableRequests] WHERE UserId = 'REPLACEGUID' Order By TimeStamp Desc
-"@
-
-                $PastLaterSQLQuery = $BasePastLaterQuery -replace 'REPLACEGUID', $User.objectGUID.Guid
+                $PastLaterSQLQuery = $BasePastLaterQuery -replace 'REPLACESID', $Request.UserId
                 [Object[]]$PastLater = Invoke-DbaQuery -SqlInstance $SqlInstance -Database $Database -Query $PastLaterSQLQuery -ErrorAction Stop
 
+                $Request.psobject.Properties.Remove('UserPolicyGroups')
                 $Now = [datetime]::Now
                 if ($null -ne $PastLater) {
+                    $ThrottleReached = $false
                     $CurrentComputerPastLater = $PastLater | Where-Object { $_.ComputerName -eq $ComputerName }
                     $CurrentComputerTimeStampString = $CurrentComputerPastLater.TimeStamp.ForEach( { $_.ToString() })
                     $CurrentComputerRequestsToday = ($CurrentComputerTimeStampString -replace '\s.*$') -match $Today
@@ -90,28 +71,25 @@ FROM [$Database].[$Schema].[$TableRequests] WHERE UserId = 'REPLACEGUID' Order B
                     $RequestsToday = ($TimeStampString -replace '\s.*$') -match $Today
 
                     if (([array]$RequestsToday).Count -gt ($Policy.TimesPerDay * $Policy.Computers)) {
-                        $ErrorNotification = 'No more requests allowed for {0} today.' -f $User.ObjectGUID.Guid
-                        $Request | Add-Member -MemberType NoteProperty -Name Error -Value $ErrorNotification -ErrorAction Stop
-                        $Request | Write-DbaDbTableData -SqlInstance $SqlInstance -Database $Database -Table $TableFailedRequests -ErrorAction Stop
-                        throw [System.AccessViolationException]::New($ErrorNotification -replace ($User.ObjectGUID.Guid, $User.Name))
+                        $ThrottleReached = $true
+                        $ErrorNotification = 'No more requests allowed for {0} today.' -f $Request.UserId
                     }
                     elseif (([array]$CurrentComputerRequestsToday).Count -gt $Policy.TimesPerDay) {
-                        $ErrorNotification = 'No more requests allowed for user {0} on computer name {1} today.' -f $User.ObjectGUID.Guid, $ComputerName
-                        $Request | Add-Member -MemberType NoteProperty -Name Error -Value $ErrorNotification -ErrorAction Stop
-                        $Request | Write-DbaDbTableData -SqlInstance $SqlInstance -Database $Database -Table $TableFailedRequests -ErrorAction Stop
-                        throw [System.AccessViolationException]::New($ErrorNotification -replace ($User.ObjectGUID.Guid, $User.Name))
+                        $ThrottleReached = $true
+                        $ErrorNotification = 'No more requests allowed for user {0} on computer name {1} today.' -f $Request.UserId, $ComputerName
                     }
                     elseif (([array]$PastLater.ComputerName | Sort-Object -Unique).Count -gt $Policy.Computers) {
-                        $ErrorNotification = 'User {0} only permitted to request for {1} computers. Limit reached, contact support.' -f $User.ObjectGUID.Guid, $Policy.Computers
-                        $Request | Add-Member -MemberType NoteProperty -Name Error -Value $ErrorNotification -ErrorAction Stop
-                        $Request | Write-DbaDbTableData -SqlInstance $SqlInstance -Database $Database -Table $TableFailedRequests -ErrorAction Stop
-                        throw [System.AccessViolationException]::New($ErrorNotification -replace ($User.ObjectGUID.Guid, $User.Name))
+                        $ThrottleReached = $true
+                        $ErrorNotification = 'User {0} only permitted to request for {1} computers. Limit reached, contact support.' -f $Request.UserId, $Policy.Computers
                     }
                     elseif ($CurrentComputerPastLater.Timestamp[0] -ge $Now.AddHours(-1)) {
-                        $ErrorNotification = 'Request already submitted for {0}, wait time {1} Minutes.' -f $User.ObjectGUID.Guid, ($PastLater[0].Timestamp - $Now.AddHours(-1)).Minutes
+                        $ThrottleReached = $true
+                        $ErrorNotification = 'Request already submitted for {0}, wait time {1} Minutes.' -f $Request.UserId, ($PastLater[0].Timestamp - $Now.AddHours(-1)).Minutes
+                    }
+                    if ($ThrottleReached) {
                         $Request | Add-Member -MemberType NoteProperty -Name Error -Value $ErrorNotification -ErrorAction Stop
                         $Request | Write-DbaDbTableData -SqlInstance $SqlInstance -Database $Database -Table $TableFailedRequests -ErrorAction Stop
-                        throw [System.AccessViolationException]::New($ErrorNotification -replace ($User.ObjectGUID.Guid, $User.Name))
+                        throw [System.AccessViolationException]::New($ErrorNotification -replace ($Request.UserId, ($PSSenderInfo.ConnectedUser -replace '^(?:.+\\)')))
                     }
                 }
                 try {
