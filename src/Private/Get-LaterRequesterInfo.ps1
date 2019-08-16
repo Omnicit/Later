06444998838e5ab9da190d87c43177a36da3da87
Philip Haglund <philip.haglund@omnicit.se>
2019-08-16 12:01:20 +0200

    Initial commit

---
+++ src/Private/Get-LaterRequesterInfo.ps1
@@ -0,0 +1,50 @@
+﻿function Get-LaterRequesterInfo {
+    [CmdletBinding(
+        SupportsShouldProcess
+    )]
+    param (
+        # Input a valid Computer Name.
+        [Parameter(
+            Mandatory
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]$ComputerName
+    )
+    process {
+        if ($PSCmdlet.ShouldProcess($ComputerName)) {
+            $WSManInstance = Get-WSManInstance -ConnectionURI $PSSenderInfo.ConnectionString -ResourceURI shell -Enumerate -ErrorAction Stop
+            try {
+                $ComputerNameByAddress = ([System.Net.Dns]::GetHostByAddress($WSManInstance.ClientIP).HostName)
+                if ($ComputerNameByAddress -match $ComputerName) {
+                    $AddressMatchName = $true
+                }
+                else {
+                    $AddressMatchName = $false
+                }
+            }
+            catch {
+                $ComputerNameByAddress = 'NULL'
+                $AddressMatchName = $false
+            }
+            $UserPolicyGroups = foreach ($Claim in $PSSenderInfo.UserInfo.WindowsIdentity.UserClaims) {
+                try {
+                    if ([Security.Principal.SecurityIdentifier]::new($Claim.Value).Translate([Security.Principal.NTAccount]).Value |
+                        Where-Object -FilterScript { $_ -match '^(?:.+\\)(res-sys-later\s(pol\d+|requester)$)' }) {
+                        $Claim.Value
+                    }
+                }
+                catch {
+                    continue
+                }
+            }
+            [PSCustomObject]@{
+                UserId                   = $PSSenderInfo.UserInfo.WindowsIdentity.User.Value
+                ComputerName             = $ComputerName
+                ComputerNameByAddress    = $ComputerNameByAddress
+                ComputerIPAddress        = $WSManInstance.ClientIP
+                ComputerNameMatchAddress = $AddressMatchName
+                UserPolicyGroups         = $UserPolicyGroups
+            }
+        }
+    }
+}
